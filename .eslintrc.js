184b96e6b283453aa051aeccc98b47e4ba30d5fe
Vadim Ilchenko <v.ilchenko@mate.academy>
2019-04-02 18:17:40 +0300

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  extends: 'standart',
+  env: {
+    node: true,
+    jest: true
+  },
+  rules: {
+    semi: ['error', 'always', {'omitLastInOneLineBlock': true}],
+    strict: 'error',
+    'no-proto': 0
+  },
+  parserOptions: {
+    sourceType: 'script'
+  },
+  plugins: ['jest']
+};
