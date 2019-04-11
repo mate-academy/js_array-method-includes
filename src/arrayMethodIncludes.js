5d133121377478ed7a2c620634ccce54c349b2c9
unknown <dumavb777@gmail.com>
2019-04-11 22:07:38 +0300

    'Solution'

--- src/arrayMethodIncludes.js
+++ src/arrayMethodIncludes.js
@@ -5,8 +5,18 @@
  *
  */
 function applyCustomIncludes() {
-  [].__proto__.includes2 = function() {
-    // write code here
+  [].__proto__.includes2 = function(isElement, index = 0) {
+    for (let i = index; i < this.length; i++) {
+      if (isNaN(isElement) && typeof isElement === 'number') {
+        if (isNaN(this[i])) {
+          return true;
+        }
+      }
+      if (this[i] === isElement) {
+        return true;
+      }
+    }
+    return false;
   };
 }
 
