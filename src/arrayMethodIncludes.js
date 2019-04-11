6ac90786c7e1eaf6fd6fcdb7a0e8db0ecc436f16
Vitalii <ilovepinkpony1@gmail.com>
2019-04-11 17:17:14 +0300

    solution

--- src/arrayMethodIncludes.js
+++ src/arrayMethodIncludes.js
@@ -7,14 +7,13 @@
 function applyCustomIncludes() {
   [].__proto__.includes2 = function(token, index = 0) {
     if (index < 0) {
-      index = index + this.length;
-      for (index; index >= 0; index--) {
-        if (this[index] === token || Number.isNaN(token)) return true;
+      for (let i = index + this.length; i >= 0; i--) {
+        if (this[i] === token || Number.isNaN(token)) return true;
       }
       return false;
     }
-    for (index; index < this.length; index++) {
-      if (this[index] === token || Number.isNaN(token)) return true;
+    for (let i = index; i < this.length; i++) {
+      if (this[i] === token || Number.isNaN(token)) return true;
     }
     return false;
   };
