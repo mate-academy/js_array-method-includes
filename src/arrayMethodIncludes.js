7ce470b22abebe048dcd36276f934ea460608925
KonstVolkovnitsky <konst.volkovnitsky@gmail.com>
2019-04-14 13:17:21 +0300

    Updated solution

--- src/arrayMethodIncludes.js
+++ src/arrayMethodIncludes.js
@@ -8,7 +8,7 @@ function applyCustomIncludes() {
   [].__proto__.includes2 = function(item = null, index = 0) {
     let result = false;
     for (let i = index; i < this.length; i++) {
-      if (this[i] === item || isNaN(item)) {
+      if (this[i] === item || (isNaN(item) && isNaN(this[i]))) {
         result = true;
       }
     }
