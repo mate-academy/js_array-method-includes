e34782e00edd953633f1274f63f05a298ef17e29
Yevhen-Kryvdiuk <krivachacha@gmail.com>
2019-04-14 15:21:29 +0300

    Yevhen Kryvdiuk

--- src/arrayMethodIncludes.js
+++ src/arrayMethodIncludes.js
@@ -4,8 +4,27 @@
  * Implement method includes
  */
 function applyCustomIncludes() {
-  [].__proto__.includes2 = function(valueToFind, fromIndex) {
-    // write code here
+  [].__proto__.includes2 = function(valueToFind, fromIndex = 0) {
+    if (fromIndex >= this.length) {
+      return false;
+    }
+    let index = 0;
+    fromIndex < 0 ? index = 0 : index = fromIndex;
+    console.log(index);
+    for (index; index < this.length; index++) {
+      if (typeof valueToFind === typeof this[index]) {
+        if (valueToFind === this[index]) {
+          return true;
+        } else if (isNaN(valueToFind) && isNaN(this[index])) {
+          return true;
+        } else {
+          continue;
+        }
+      } else {
+        continue;
+      }
+    }
+    return false;
   };
 }
 
