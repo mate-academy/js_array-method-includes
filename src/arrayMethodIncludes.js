bacb5b57293fdb4b9ac8d647cbb5300a4fc09827
gestap <gestap@gmail.com>
2019-04-15 13:31:39 +0300

    Solution

--- src/arrayMethodIncludes.js
+++ src/arrayMethodIncludes.js
@@ -5,7 +5,9 @@
  */
 function applyCustomIncludes() {
   [].__proto__.includes2 = function(valueToFind, fromIndex) {
-    // write code here
+    return fromIndex === undefined
+      ? this.some(el => Object.is(el, valueToFind))
+      : this.slice(fromIndex).some(el => Object.is(el, valueToFind));
   };
 }
 
