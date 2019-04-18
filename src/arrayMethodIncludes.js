8e6c41b4c4fbe58d01639cafac9758a1d674fd70
valeriitovstyk <valeriitovstyk@gmail.com>
2019-04-18 12:16:43 +0300

    solution for array method includes by @laverka

--- src/arrayMethodIncludes.js
+++ src/arrayMethodIncludes.js
@@ -4,8 +4,19 @@
  * Implement method includes
  */
 function applyCustomIncludes() {
-  [].__proto__.includes2 = function(valueToFind, fromIndex) {
-    // write code here
+  [].__proto__.includes2 = function(valueToFind, fromIndex = 0) {
+    const arrayLength = this.length;
+    if (arrayLength === 0) return false;
+    if (fromIndex >= this.length) return false;
+    if (valueToFind === undefined) return false;
+
+    for (let i = fromIndex; i < this.length; i++) {
+      if (isNaN(valueToFind) && (isNaN(valueToFind) === isNaN(this[i]))) {
+        return true;
+      }
+      if (valueToFind === this[i]) return true;
+    }
+    return false;
   };
 }
 
