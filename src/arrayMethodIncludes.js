15b994915efc30d591857f37a1bb455256cdd4c6
valeriitovstyk <valeriitovstyk@gmail.com>
2019-04-21 22:39:49 +0300

    separated IF conditions in one chain, simplified NaN check

--- src/arrayMethodIncludes.js
+++ src/arrayMethodIncludes.js
@@ -6,12 +6,16 @@
 function applyCustomIncludes() {
   [].__proto__.includes2 = function(valueToFind, fromIndex = 0) {
     const arrayLength = this.length;
-    if (arrayLength === 0) return false;
-    if (fromIndex >= this.length) return false;
-    if (valueToFind === undefined) return false;
+    if (
+      arrayLength === 0
+      || fromIndex >= this.length // eslint-disable-line
+      || valueToFind === undefined  // eslint-disable-line
+    ) {
+      return false;
+    }
 
     for (let i = fromIndex; i < this.length; i++) {
-      if (isNaN(valueToFind) && (isNaN(valueToFind) === isNaN(this[i]))) {
+      if (isNaN(valueToFind) && isNaN(this[i])) {
         return true;
       }
       if (valueToFind === this[i]) return true;
