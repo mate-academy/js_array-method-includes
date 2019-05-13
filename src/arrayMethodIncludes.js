817b52f53a8a51727b8bf25ea53149503a640fe9
Ivan Deiko <ivandeiko1997@gmail.com>
2019-05-13 23:01:16 +0300

    'solution'

--- src/arrayMethodIncludes.js
+++ src/arrayMethodIncludes.js
@@ -5,8 +5,18 @@
  *
  */
 function applyCustomIncludes() {
-  [].__proto__.includes2 = function() {
-    // write code here
+  [].__proto__.includes2 = function(elem, index = 0) {
+    if (elem === undefined) {
+      return false;
+    }
+    for (index; index < this.length; index++) {
+      if (this[index] === elem) {
+        return true;
+      } else if (isNaN(this[index]) && isNaN(elem)) {
+        return true;
+      }
+    }
+    return false;
   };
 }
 
