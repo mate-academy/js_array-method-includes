14bcdf25d645f4cb832c2844300a8f4c0b6a6a3d
OleksandrHlybchenko <logista@meta.ua>
2019-06-19 03:15:52 +0300

    for review

--- src/arrayMethodIncludes.js
+++ src/arrayMethodIncludes.js
@@ -5,7 +5,18 @@
  */
 function applyCustomIncludes() {
   [].__proto__.includes2 = function(valueToFind, fromIndex) {
-    // write code here
+    if (valueToFind === undefined || this.length < 1) {
+      return false;
+    }
+
+    for (let i = fromIndex || 0; i < this.length; i++) {
+      if (this[i] === valueToFind) {
+        return true;
+      } else if (isNaN(this[i]) && isNaN(valueToFind)) {
+        return true;
+      }
+    }
+    return false;
   };
 }
 
