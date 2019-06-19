2198df00a3b1d1d5451819500989b18d3a8f81f1
Dmitry Yevlakhov <tragenstolz@gmail.com>
2019-06-19 15:05:34 +0300

    includes

--- src/arrayMethodIncludes.js
+++ src/arrayMethodIncludes.js
@@ -4,8 +4,22 @@
  * Implement method includes
  */
 function applyCustomIncludes() {
-  [].__proto__.includes2 = function(valueToFind, fromIndex) {
-    // write code here
+  [].__proto__.includes2 = function(valueToFind, fromIndex = 0) {
+    if (valueToFind === undefined) {
+      return false;
+    }
+
+    for (let i = fromIndex || 0; i < this.length; i++) {
+      if (this[i] === valueToFind) {
+        return true;
+      }
+
+      if (isNaN(this[i]) && isNaN(valueToFind)) {
+        return true;
+      }
+    }
+
+    return false;
   };
 }
 
