e43e9e5e9f7e284defae13bbe2a1f0946aeb67fb
RavenVol <r.volianiuk@gmail.com>
2019-06-20 11:17:09 +0300

    Includes2 - Solved by Raven

--- src/arrayMethodIncludes.js
+++ src/arrayMethodIncludes.js
@@ -6,6 +6,33 @@
 function applyCustomIncludes() {
   [].__proto__.includes2 = function(valueToFind, fromIndex) {
     // write code here
+    if (!fromIndex) {
+      fromIndex = 0;
+    }
+
+    if (fromIndex > this.length - 1) {
+      return undefined;
+    }
+
+    if (valueToFind === undefined) {
+      return false;
+    }
+
+    for (let i = fromIndex; i < this.length; i++) {
+      if (this[i] === valueToFind) {
+        return true;
+      }
+
+      if (isNaN(this[i])
+          && typeof(this[i]) === 'number'
+          && isNaN(valueToFind)
+          && typeof(valueToFind) === 'number')
+      {
+        return true;
+      }
+    }
+
+    return false;
   };
 }
 
