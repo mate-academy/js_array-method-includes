d1d76c88e960684038e062e7bd20d074f2aebdb5
Пользователь <Sviatoslv>
2019-06-21 12:28:57 +0300

    ValueToFind Negative Number check added

--- src/arrayMethodIncludes.js
+++ src/arrayMethodIncludes.js
@@ -5,9 +5,15 @@
  */
 function applyCustomIncludes() {
   [].__proto__.includes2 = function(valueToFind, fromIndex = 0) {
+    let findIndex = valueToFind;
+
+    if (findIndex < 0) {
+      findIndex = this.length + valueToFind;
+    }
+
     for (let i = fromIndex; i < this.length; i++) {
-      if (this[i] === valueToFind
-        || (Number.isNaN(this[i]) && Number.isNaN(valueToFind))) {
+      if (this[i] === findIndex
+        || (Number.isNaN(this[i]) && Number.isNaN(findIndex))) {
         return true;
       }
     }
