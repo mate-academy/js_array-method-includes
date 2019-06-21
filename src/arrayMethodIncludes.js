c0df6b14ef31578795735345b6628688ff223d7d
Kamran <djkamry@gmail.com>
2019-06-21 18:50:15 +0300

    Solution

--- src/arrayMethodIncludes.js
+++ src/arrayMethodIncludes.js
@@ -4,8 +4,27 @@
  * Implement method includes
  */
 function applyCustomIncludes() {
-  [].__proto__.includes2 = function(valueToFind, fromIndex) {
-    // write code here
+  [].__proto__.includes2 = function(valueToFind, fromIndex = 0) {
+    if (valueToFind === undefined) {
+      return false;
+    }
+
+    let firstIndex;
+    if (fromIndex < 0) {
+      firstIndex = this.length - 1 + fromIndex;
+    } else {
+      firstIndex = fromIndex;
+    }
+
+    for (let i = firstIndex; i < this.length; i++) {
+      if (this[i] === valueToFind || this[i] === undefined) {
+        return true;
+      }
+      if (isNaN(this[i]) && isNaN(valueToFind)) {
+        return true;
+      }
+    }
+    return false;
   };
 }
 
