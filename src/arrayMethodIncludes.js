2c64f7df690f8762a007ec4c7e217cc6bc1a263a
Taisiia <taisiia.pomazan@gmail.com>
2019-06-21 15:19:55 +0300

    sol

--- src/arrayMethodIncludes.js
+++ src/arrayMethodIncludes.js
@@ -4,8 +4,26 @@
  * Implement method includes
  */
 function applyCustomIncludes() {
-  [].__proto__.includes2 = function(valueToFind, fromIndex) {
-    // write code here
+  [].__proto__.includes2 = function(valueToFind, fromIndex = 0) {
+    let index;
+
+    if (fromIndex < 0) {
+      index = this.length + fromIndex;
+    } else {
+      index = fromIndex;
+    };
+
+    for (index; index < this.length; index++) {
+      if (
+        this[index] === valueToFind
+        || (Number.isNaN(this[index])
+        && Number.isNaN(valueToFind))
+      ) {
+        return true;
+      };
+    };
+
+    return false;
   };
 }
 
