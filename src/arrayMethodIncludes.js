b74b410ccdc9f97c9e1710caea04631ea57186a0
Sergii Kasian <kasyansv1@gmail.com>
2019-06-21 10:02:16 +0300

    solution

--- src/arrayMethodIncludes.js
+++ src/arrayMethodIncludes.js
@@ -4,8 +4,17 @@
  * Implement method includes
  */
 function applyCustomIncludes() {
-  [].__proto__.includes2 = function(valueToFind, fromIndex) {
-    // write code here
+  [].__proto__.includes2 = function(valueToFind, fromIndex = 0) {
+    const value = valueToFind;
+
+    for (let i = fromIndex; i < this.length; i++) {
+      if (valueToFind === this[i]
+        || valueToFind !== value
+        || valueToFind === '') {
+        return true;
+      }
+      return false;
+    }
   };
 }
 
