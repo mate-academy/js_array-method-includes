b640ff1ec4469630a10fb359a65c6d4895bf43ad
Yevgeniy <zleonenkov@gmail.com>
2019-06-21 12:25:14 +0300

    fromIndex fixed

--- src/arrayMethodIncludes.js
+++ src/arrayMethodIncludes.js
@@ -4,8 +4,16 @@
  * Implement method includes
  */
 function applyCustomIncludes() {
-  [].__proto__.includes2 = function(valueToFind, fromIndex) {
-    for (let i = fromIndex || 0; i < this.length; i++) {
+  [].__proto__.includes2 = function(valueToFind, fromIndex = 0) {
+    let i = 0;
+
+    if (fromIndex < 0) {
+      i = this.length - fromIndex;
+    } else {
+      i = fromIndex;
+    }
+
+    for (i; i < this.length; i++) {
       if ((this[i] === valueToFind)
         || (Number.isNaN(this[i]) && Number.isNaN(valueToFind))) {
         return true;
