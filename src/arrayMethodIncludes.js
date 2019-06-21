6cc9e36b7067b1f897e43f303fbd67e6ffad8298
Serhii <davydko.serhiy@gmail.com>
2019-06-21 11:51:49 +0300

    add index checker

--- src/arrayMethodIncludes.js
+++ src/arrayMethodIncludes.js
@@ -5,18 +5,19 @@
  */
 function applyCustomIncludes() {
   [].__proto__.includes2 = function(valueToFind = 0, fromIndex = 0) {
-    let result = false;
-    for (let i = fromIndex; i < this.length; i++) {
+    let calcIndex = fromIndex;
+    if (calcIndex < 0) {
+      calcIndex = Math.max(this.length + calcIndex, 0);
+    }
+    for (let i = calcIndex; i < this.length; i++) {
       if (isNaN(valueToFind) && isNaN(this[i])) {
-        result = true;
-        return result;
+        return true;
       }
       if (valueToFind === this[i]) {
-        result = true;
-        return result;
+        return true;
       }
     }
-    return result;
+    return false;
     // write code here
   };
 }
