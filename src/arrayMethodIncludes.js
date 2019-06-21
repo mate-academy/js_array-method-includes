277e9c4d2e41116cbe6475aaae8c53a2ae8b3d93
IvanVFedenko <ivan.v.fedenko@gmail.com>
2019-06-21 12:23:10 +0300

    negative fromIndex
    negative fromIndex

--- src/arrayMethodIncludes.js
+++ src/arrayMethodIncludes.js
@@ -5,20 +5,23 @@
 function applyCustomIncludes() {
   [].__proto__.includes2 = function(valueToFind, fromIndex) {
     let searchIndex = fromIndex;
+    if (searchIndex < 0) {
+      searchIndex += this.length;
+    };
     for (let i = 0; i < this.length; i++) {
       if (!fromIndex) {
         searchIndex = i;
-      }
-      if (arguments.length < 1) {
+      };
+      if (valueToFind === undefined) {
         return false;
-      }
+      };
       if (isNaN(this[i]) === true && isNaN(valueToFind) === true) {
         return true;
-      }
+      };
       if (this[i] === valueToFind && searchIndex === i) {
         return true;
-      }
-    }
+      };
+    };
     return false;
   };
 };
