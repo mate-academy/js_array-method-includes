03f8e211f984db74ff1aab64325e62dd0ace9c68
Sergii Kasian <kasyansv1@gmail.com>
2019-06-21 11:43:33 +0300

    edited solution2

--- src/arrayMethodIncludes.js
+++ src/arrayMethodIncludes.js
@@ -6,19 +6,19 @@
 function applyCustomIncludes() {
   [].__proto__.includes2 = function(valueToFind, fromIndex = 0) {
     const value = valueToFind;
+    let valueRules = false;
+    if (valueToFind !== value || valueToFind === '') {
+      valueRules = true;
+    }
     if (fromIndex < 0) {
       for (let i = this.length + fromIndex; i >= 0; i--) {
-        if (valueToFind === this[i]
-          || valueToFind !== value
-          || valueToFind === '') {
+        if (valueToFind === this[i] || valueRules) {
           return true;
         }
       }
     } else {
       for (let i = fromIndex; i < this.length; i++) {
-        if (valueToFind === this[i]
-          || valueToFind !== value
-          || valueToFind === '') {
+        if (valueToFind === this[i] || valueRules) {
           return true;
         }
       }
