18cb99d28f1d60018435fc1c8af8d88273e4267e
neitiris <veliantos@gmail.com>
2019-06-21 17:23:23 +0300

    solution fix

--- src/arrayMethodIncludes.js
+++ src/arrayMethodIncludes.js
@@ -6,7 +6,8 @@
 function applyCustomIncludes() {
   [].__proto__.includes2 = function(valueToFind, fromIndex = 0) {
     // write code here
-    const startIter = fromIndex < 0 ? this.length - fromIndex : fromIndex;
+    const startIter = fromIndex < 0
+      ? fromIndex + this.length : fromIndex;
     for (let i = startIter; i < this.length; i++) {
       if (this[i] === valueToFind) {
         return true;
