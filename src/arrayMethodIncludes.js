ef372b20dbaf6905faa94dc8c571a2c07e6b4761
Vitaliy <mpvitalik@users.noreply.github.com>
2019-06-22 21:35:00 +0300

    check "-index"

--- src/arrayMethodIncludes.js
+++ src/arrayMethodIncludes.js
@@ -5,6 +5,10 @@
  */
 function applyCustomIncludes() {
   [].__proto__.includes2 = function(valueToFind, fromIndex = 0) {
+    let index = valueToFind;
+    if (index < 0) {
+      index = this.length + valueToFind;
+    }
     for (let i = fromIndex; i < this.length; i++) {
       if (this[i] === valueToFind
         || (Number.isNaN(this[i])
