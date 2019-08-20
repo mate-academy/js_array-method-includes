54e63e02621664f5fb5f7eaae2a5f9ff5c1d15a3
yevhen-sahach <antitop04@gmail.com>
2019-08-21 01:10:56 +0300

    first commit

--- src/arrayMethodIncludes.js
+++ src/arrayMethodIncludes.js
@@ -5,7 +5,25 @@
  */
 function applyCustomIncludes() {
   [].__proto__.includes2 = function(valueToFind, fromIndex) {
-    // write code here
+    if (Number.isNaN(valueToFind)) {
+      return true;
+    }
+
+    let i;
+
+    if (typeof fromIndex === 'number') {
+      i = fromIndex;
+    } else {
+      i = 0;
+    }
+
+    for (i; i < this.length; i++) {
+      if (this[i] === valueToFind) {
+        return true;
+      }
+    }
+
+    return false;
   };
 }
 
