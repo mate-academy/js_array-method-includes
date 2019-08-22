73af39ecdba44ec45af4b07f23d4688aca31d58c
Khrystyna Styslo <khrystyna.styslo@gmail.com>
2019-08-22 12:04:16 +0300

    fixed isNaN

--- src/arrayMethodIncludes.js
+++ src/arrayMethodIncludes.js
@@ -4,12 +4,10 @@
  * Implement method includes
  */
 function applyCustomIncludes() {
-  [].__proto__.includes2 = function(valueToFind, fromIndex) {
-    let i = arguments.length < 2 ? 0 : fromIndex;
-    const value = arguments.length < 1 ? true : valueToFind;
-
-    for (i; i < this.length; i++) {
-      if (value === this[i] || (isNaN(value) && isNaN(this[i]))) {
+  [].__proto__.includes2 = function(valueToFind, fromIndex = 0) {
+    for (let i = fromIndex; i < this.length; i++) {
+      if (valueToFind === this[i]
+        || (Number.isNaN(valueToFind) && Number.isNaN(this[i]))) {
         return true;
       }
     }
