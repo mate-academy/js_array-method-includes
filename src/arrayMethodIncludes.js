b65dc5a2058d5b4b3847da5d58a012a2821d606b
Viktor Pastushenko <pastushenko.vik@gmail.com>
2019-08-22 19:09:55 +0300

    flat code

--- src/arrayMethodIncludes.js
+++ src/arrayMethodIncludes.js
@@ -5,15 +5,17 @@
  */
 function applyCustomIncludes() {
   [].__proto__.includes2 = function(valueToFind, fromIndex = 0) {
-    if (arguments.length !== 0) {
-      let i = fromIndex;
-      while (i < this.length) {
-        if (this[i] === valueToFind
-          || (isNaN(valueToFind) && isNaN(this[i]))) {
-          return true;
-        }
-        i++;
+    if (arguments.length === 0) {
+      return false;
+    }
+
+    let i = fromIndex;
+    while (i < this.length) {
+      if (this[i] === valueToFind
+        || (isNaN(valueToFind) && isNaN(this[i]))) {
+        return true;
       }
+      i++;
     }
 
     return false;
