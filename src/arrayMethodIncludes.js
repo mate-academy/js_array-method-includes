34ef21fa26b1510eb669b7dcd21075dda0772f1a
Vyacheslav Piskun <webdev.pv@gmail.com>
2019-08-23 22:06:06 +0300

    solution

--- src/arrayMethodIncludes.js
+++ src/arrayMethodIncludes.js
@@ -4,8 +4,29 @@
  * Implement method includes
  */
 function applyCustomIncludes() {
-  [].__proto__.includes2 = function(valueToFind, fromIndex) {
-    // write code here
+  [].__proto__.includes2 = function(valueToFind, fromIndex = 0) {
+    let index = fromIndex;
+
+    if (index < 0) {
+      index = this.length + index;
+    }
+
+    if (index < 0) {
+      index = 0;
+    }
+
+    for (let i = index; i < this.length; i++) {
+      // eslint-disable-next-line no-self-compare
+      if (this[i] !== this[i] && valueToFind !== valueToFind) {
+        return true;
+      }
+
+      if (this[i] === valueToFind) {
+        return true;
+      }
+    };
+
+    return false;
   };
 }
 
