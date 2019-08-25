780d3cd69f2d1088f2d0fac97f0fbe85f3e5a0b5
KosBelozyorov <KosBelozyorov@gmail.com>
2019-08-25 21:54:34 +0300

    Soution

--- src/arrayMethodIncludes.js
+++ src/arrayMethodIncludes.js
@@ -4,8 +4,19 @@
  * Implement method includes
  */
 function applyCustomIncludes() {
-  [].__proto__.includes2 = function(valueToFind, fromIndex) {
-    // write code here
+  [].__proto__.includes2 = function(valueToFind, fromIndex = 0) {
+    let result = false;
+    for (let i = fromIndex; i < this.length; i += 1) {
+      if (this[i] === valueToFind) {
+        result = true;
+      }
+
+      if (Number.isNaN(valueToFind) && Number.isNaN(this[i])) {
+        result = true;
+      }
+    }
+
+    return result;
   };
 }
 
