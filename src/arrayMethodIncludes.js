555b9848b6ff5436e16171e7d646b2cd01814086
Volodsher <volodsher@gmail.com>
2019-08-26 19:33:30 +0300

    it took quite longer than others

--- src/arrayMethodIncludes.js
+++ src/arrayMethodIncludes.js
@@ -3,9 +3,22 @@
 /**
  * Implement method includes
  */
+
 function applyCustomIncludes() {
   [].__proto__.includes2 = function(valueToFind, fromIndex) {
-    // write code here
+    let i = 0;
+    if (fromIndex) {
+      i = fromIndex;
+    };
+    for (i; i < this.length; i++) {
+      // eslint-disable-next-line no-self-compare
+      if ((valueToFind !== valueToFind) && (this[i] !== this[i])) {
+        return true;
+      } else if (this[i] === valueToFind) {
+        return true;
+      }
+    }
+    return false;
   };
 }
 
