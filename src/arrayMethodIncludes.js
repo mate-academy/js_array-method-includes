89d247ed8e1f8139ed979c94cac24548f1834b8d
vladyslav223 <vladyslav223@gmail.com>
2019-08-27 20:54:48 +0300

    includes done

--- src/arrayMethodIncludes.js
+++ src/arrayMethodIncludes.js
@@ -6,6 +6,15 @@
 function applyCustomIncludes() {
   [].__proto__.includes2 = function(valueToFind, fromIndex) {
     // write code here
+    for (let i = fromIndex; i < this.length; i++) {
+      if (this[i] === valueToFind
+        || (Number.isNaN(this[i])
+        && Number.isNaN(valueToFind))) {
+        return true;
+      }
+    }
+
+    return false;
   };
 }
 
