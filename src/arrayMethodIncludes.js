9ff444a9c11f05eb233d7e6190e751df43693227
Miroslava <777shadowpriest777@gmail.com>
2019-08-27 20:08:38 +0300

    refactored

--- src/arrayMethodIncludes.js
+++ src/arrayMethodIncludes.js
@@ -5,11 +5,10 @@
  */
 function applyCustomIncludes() {
   [].__proto__.includes2 = function(valueToFind, fromIndex) {
-    if (valueToFind === undefined || this.length === 0) {
+    if (arguments.length === 0) {
       return false;
     }
 
-    let result = false;
     let i = 0;
 
     if (fromIndex !== undefined) {
@@ -18,16 +17,14 @@ function applyCustomIncludes() {
 
     for (i; i < this.length; i++) {
       if (isNaN(this[i]) && isNaN(valueToFind)) {
-        result = true;
-        break;
+        return true;
       }
       if (this[i] === valueToFind) {
-        result = true;
-        break;
+        return true;
       }
     }
 
-    return result;
+    return false;
   };
 }
 
