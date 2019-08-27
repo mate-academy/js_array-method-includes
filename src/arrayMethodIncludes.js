e3ae982078f71878a391c4f197a2927272bf708e
unknown <shklyarigor2015@gmail.com>
2019-08-27 13:02:45 +0300

    fix

--- src/arrayMethodIncludes.js
+++ src/arrayMethodIncludes.js
@@ -4,15 +4,10 @@
  * Implement method includes
  */
 function applyCustomIncludes() {
-  [].__proto__.includes2 = function(valueToFind, fromIndex) {
-    const sourseIndex = (fromIndex !== undefined) ? fromIndex : 0;
-
-    if (valueToFind === undefined) {
-      return false;
-    }
-
-    for (let i = sourseIndex; i < this.length - 1; i++) {
-      if (this[i] === valueToFind || (isNaN(valueToFind))) {
+  [].__proto__.includes2 = function(valueToFind, fromIndex = 0) {
+    for (let i = fromIndex; i < this.length - 1; i++) {
+      if (this[i] === valueToFind
+        || (isNaN(this[i]) && Number.isNaN(valueToFind))) {
         return true;
       }
     }
