3de7427e82d5ea3941bdf012bedc6d0328a6a0c8
aleks-mos <mossssss@ukr.net>
2019-08-27 13:09:37 +0300

    change doc

--- src/arrayMethodIncludes.js
+++ src/arrayMethodIncludes.js
@@ -4,23 +4,17 @@
  * Implement method includes
  */
 function applyCustomIncludes() {
-  [].__proto__.includes2 = function(valueToFind, fromIndex) {
-    let i;
-
-    if (typeof fromIndex === 'number') {
-      i = fromIndex;
-    } else {
-      i = 0;
-    }
-
-    for (i; i < this.length; i++) {
+  [].__proto__.includes2 = function(valueToFind, fromIndex = 0) {
+    for (let i = fromIndex; i < this.length; i++) {
       if (isNaN(this[i]) && Number.isNaN(valueToFind)) {
         return true;
       }
+
       if (this[i] === valueToFind) {
         return true;
       }
     }
+
     return false;
   };
 }
