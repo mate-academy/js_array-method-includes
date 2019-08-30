6adf0ae07b2b0624bf08a3cb3bfce42a12415da8
Illia Bezkorovainyi <illia.bezkorovainyi@heathmont.net>
2019-08-30 11:41:15 +0300

    Solution

--- src/arrayMethodIncludes.js
+++ src/arrayMethodIncludes.js
@@ -5,7 +5,26 @@
  */
 function applyCustomIncludes() {
   [].__proto__.includes2 = function(valueToFind, fromIndex) {
-    // write code here
+    if (valueToFind === undefined || this.length === 0) {
+      return false;
+    }
+    let i;
+    if (fromIndex >= 0) {
+      i = fromIndex;
+    } else if (fromIndex < 0) {
+      i = this.length + fromIndex;
+    } else if (!fromIndex) {
+      i = 0;
+    }
+    for (i; i < this.length; i++) {
+      if (isNaN(valueToFind) && isNaN(this[i])) {
+        return true;
+      } else if (valueToFind === this[i]) {
+        return true;
+      }
+    }
+
+    return false;
   };
 }
 
