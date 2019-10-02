1ccbc0b5628aee44761715a30039425bd0c1f17d
Serhii <gespenst.serge@yandex.ru>
2019-10-02 18:31:44 +0300

    sol

--- src/arrayMethodIncludes.js
+++ src/arrayMethodIncludes.js
@@ -5,7 +5,19 @@
  */
 function applyCustomIncludes() {
   [].__proto__.includes2 = function(valueToFind, fromIndex) {
-    // write code here
+    let startFrom = fromIndex;
+    if (startFrom === undefined) {
+      startFrom = 0;
+    }
+
+    for (let i = startFrom; i < this.length; i++) {
+      if (this[i] === valueToFind || (Number.isNaN(this[i]) && Number
+        .isNaN(valueToFind))) {
+        return true;
+      }
+    }
+
+    return false;
   };
 }
 
