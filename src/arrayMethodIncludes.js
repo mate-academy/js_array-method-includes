57716dd01b703dde31b5dbba7cc5f8a4300a75d1
natya2203 <nastya_2203@ukr.net>
2019-11-19 15:53:36 +0200

    solution

--- src/arrayMethodIncludes.js
+++ src/arrayMethodIncludes.js
@@ -5,17 +5,25 @@
  */
 function applyCustomIncludes() {
   [].__proto__.includes2 = function(valueToFind, fromIndex = 0) {
-    if (fromIndex >= 0) {
-      for (let i = fromIndex; i < this.length; i++) {
-        if (isNaN(valueToFind) === isNaN(NaN) && valueToFind !== undefined) {
-          return true;
-        }
-
-        if (this[i] === valueToFind) {
-          return true;
-        }
+    if (fromIndex < 0) {
+      // eslint-disable-next-line no-cond-assign,no-param-reassign
+      fromIndex = fromIndex + [].length;
+      if (fromIndex < 0) {
+        // eslint-disable-next-line no-param-reassign
+        fromIndex = 0;
       }
     }
+
+    for (let i = fromIndex; i < this.length; i++) {
+      if (isNaN(valueToFind) === isNaN(NaN) && valueToFind !== undefined) {
+        return true;
+      }
+
+      if (this[i] === valueToFind) {
+        return true;
+      }
+    }
+
     return false;
   };
 }
