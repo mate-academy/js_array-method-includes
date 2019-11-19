00405296cc8ddf7157eac0b94b2654c9cbf0995b
Inna <inna.tereshchenko1@gmail.com>
2019-11-19 16:31:53 +0200

    solution

--- src/arrayMethodIncludes.js
+++ src/arrayMethodIncludes.js
@@ -5,6 +5,15 @@
  */
 function applyCustomIncludes() {
   [].__proto__.includes2 = function(valueToFind, fromIndex = 0) {
+    if (fromIndex < 0) {
+      for (let i = fromIndex; i < 0; i++) {
+        if (Object.is(this[i], valueToFind)) {
+          return true;
+        }
+      }
+      return false;
+    }
+
     for (let i = fromIndex; i < this.length; i++) {
       if (Object.is(this[i], valueToFind)) {
         return true;
