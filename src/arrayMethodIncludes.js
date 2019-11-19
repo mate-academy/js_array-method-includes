ca645d805e17340961760eb437e9e3769d4d7491
ksukhaldun <kseniya.khaldun@gmail.com>
2019-11-19 17:24:34 +0300

    include

--- src/arrayMethodIncludes.js
+++ src/arrayMethodIncludes.js
@@ -5,10 +5,16 @@
  */
 function applyCustomIncludes() {
   [].__proto__.includes2 = function(valueToFind, fromIndex = 0) {
+    if (fromIndex < 0) {
+      for (let i = this.length - fromIndex; i < this.length; i++) {
+        return true;
+      }
+    }
+
     for (let i = fromIndex; i < this.length; i++) {
       if (this[i] === valueToFind || (isNaN(valueToFind) && isNaN(this[i]))) {
         return true;
-      } else if (valueToFind === undefined) {
+      } else if (valueToFind === undefined || fromIndex < 0) {
         return false;
       }
     }
