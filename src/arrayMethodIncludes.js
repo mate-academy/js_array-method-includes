78e87fe86038baeffcaf344deafb5ffa165398de
YuliiaChupryna <julia.chuprina2907@gmail.com>
2019-11-19 15:24:59 +0200

    solution

--- src/arrayMethodIncludes.js
+++ src/arrayMethodIncludes.js
@@ -9,11 +9,21 @@ function applyCustomIncludes() {
       return false;
     }
 
-    for (let i = fromIndex; i < this.length; i++) {
-      if ((valueToFind === this[i])
-        || ((valueToFind.toString() === 'NaN')
-          && (this[i].toString() === 'NaN'))) {
-        return true;
+    if (fromIndex >= 0) {
+      for (let i = fromIndex; i < this.length; i++) {
+        if ((valueToFind === this[i])
+          || ((valueToFind.toString() === 'NaN')
+            && (this[i].toString() === 'NaN'))) {
+          return true;
+        }
+      }
+    } else {
+      for (let i = this.length + fromIndex; i < this.length; i++) {
+        if ((valueToFind === this[i])
+          || ((valueToFind.toString() === 'NaN')
+            && (this[i].toString() === 'NaN'))) {
+          return true;
+        }
       }
     }
     return false;
