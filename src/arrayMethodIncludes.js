c19922ccdfc3c5efe34b4f509da846985096d5bc
art1shok <kvitkaart554@gmail.com>
2019-11-19 16:58:37 +0200

    solution

--- src/arrayMethodIncludes.js
+++ src/arrayMethodIncludes.js
@@ -5,15 +5,17 @@
  */
 function applyCustomIncludes() {
   [].__proto__.includes2 = function(valueToFind, fromIndex = 0) {
-    for (let i = fromIndex; i < this.length; i++) {
-      if (Number.isNaN(this[i]) && Number.isNaN(valueToFind)) {
-        return true;
-      } else if (this[i] === valueToFind) {
-        return true;
+    if (fromIndex >= 0) {
+      for (let i = fromIndex; i < this.length; i++) {
+        if (Number.isNaN(this[i]) && Number.isNaN(valueToFind)) {
+          return true;
+        } else if (this[i] === valueToFind) {
+          return true;
+        }
       }
-    }
 
-    return false;
+      return false;
+    }
   };
 }
 
