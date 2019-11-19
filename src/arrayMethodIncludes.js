87f04f46b9298434b8c9db15d0b7bf5454c47080
Denys <denys.cheporniuk@gmail.com>
2019-11-19 14:36:13 +0200

    fix

--- src/arrayMethodIncludes.js
+++ src/arrayMethodIncludes.js
@@ -4,24 +4,31 @@
  * Implement method includes
  */
 function applyCustomIncludes() {
-  [].__proto__.includes2 = function(valueToFind, fromIndex) {
+  [].__proto__.includes2 = function(valueToFind, fromIndex = 0) {
     if (valueToFind === undefined) {
       return false;
     }
 
-    if (fromIndex && this[fromIndex] !== valueToFind) {
-      return false;
-    }
-
-    for (let i = 0; i < this.length; i++) {
-      if (this[i].toString() === valueToFind.toString()
-          && typeof this[i] === typeof valueToFind) {
-        return true;
+    if (fromIndex >= 0) {
+      for (let i = fromIndex; i < this.length; i++) {
+        if (valueToFind === this[i]
+            || (isNaN(valueToFind)
+            && isNaN(valueToFind) === isNaN(this[i]))) {
+          return true;
+        }
+      }
+    } else {
+      for (let i = this.length + fromIndex; i < this.length; i++) {
+        if (valueToFind === this[i]
+            || (isNaN(valueToFind)
+            && isNaN(valueToFind) === isNaN(this[i]))) {
+          return true;
+        }
       }
     }
 
     return false;
   };
-}
+};
 
 module.exports = applyCustomIncludes;
