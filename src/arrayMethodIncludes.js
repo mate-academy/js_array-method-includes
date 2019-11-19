fd073666ad03a14d8c4e586d331b336a52c53818
helmax-y <max_rad@ukr.net>
2019-11-19 18:00:14 +0200

    Solution

--- src/arrayMethodIncludes.js
+++ src/arrayMethodIncludes.js
@@ -5,7 +5,27 @@
  */
 function applyCustomIncludes() {
   [].__proto__.includes2 = function(valueToFind, fromIndex) {
-    // write code here
+    if (valueToFind === undefined) {
+      return false;
+    }
+
+    if (isFinite(fromIndex)) {
+      for (let i = fromIndex; i < this.length; i++) {
+        if ((!isNaN(this[i]) && this[i] === valueToFind)
+          || (isNaN(this[i]) && isNaN(valueToFind))) {
+          return true;
+        }
+      }
+    } else {
+      for (const value of this) {
+        if ((!isNaN(value) && value === valueToFind)
+          || (isNaN(value) && isNaN(valueToFind))) {
+          return true;
+        }
+      }
+    }
+
+    return false;
   };
 }
 
