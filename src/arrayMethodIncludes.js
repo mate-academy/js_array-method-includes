97287bdfdc50a1b4f9af2d1055e3f84a70c93257
Stanislav Mochyr <delenjer@gmail.com>
2019-11-19 19:15:19 +0200

    Solutio

--- src/arrayMethodIncludes.js
+++ src/arrayMethodIncludes.js
@@ -4,8 +4,30 @@
  * Implement method includes
  */
 function applyCustomIncludes() {
-  [].__proto__.includes2 = function(valueToFind, fromIndex) {
-    // write code here
+  [].__proto__.includes2 = function(valueToFind, fromIndex = 0) {
+    if (valueToFind === undefined) {
+      return false;
+    }
+
+    if (fromIndex >= 0) {
+      for (let i = fromIndex; i < this.length; i++) {
+        if (this[i] === valueToFind
+          || (isNaN(valueToFind)
+            && isNaN(valueToFind) === isNaN(this[i]))) {
+          return true;
+        }
+      }
+    } else {
+      for (let i = this.length + fromIndex; i < this.length; i++) {
+        if (this[i] === valueToFind
+          || (isNaN(valueToFind)
+            && isNaN(valueToFind) === isNaN(this[i]))) {
+          return true;
+        }
+      }
+    }
+
+    return false;
   };
 }
 
