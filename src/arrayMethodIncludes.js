a8daf9b2b67c28f6efadc6cd83a0cd8f4e0a7571
Denys <denys.cheporniuk@gmail.com>
2019-11-19 10:41:45 +0200

    solution

--- src/arrayMethodIncludes.js
+++ src/arrayMethodIncludes.js
@@ -5,7 +5,22 @@
  */
 function applyCustomIncludes() {
   [].__proto__.includes2 = function(valueToFind, fromIndex) {
-    // write code here
+    if (valueToFind === undefined) {
+      return false;
+    }
+
+    if (fromIndex && this[fromIndex] !== valueToFind) {
+      return false;
+    }
+
+    for (let i = 0; i < this.length; i++) {
+      if (this[i].toString() === valueToFind.toString()
+          && typeof this[i] === typeof valueToFind) {
+        return true;
+      }
+    }
+
+    return false;
   };
 }
 
