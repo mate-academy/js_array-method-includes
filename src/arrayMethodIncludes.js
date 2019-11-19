7cbd1ad5299b88bec22ff21e64d49702a32da31a
Alexii Aidinov <alexii.aidinov@gmail.com>
2019-11-19 21:49:48 +0200

    Aidinov Alexii

--- src/arrayMethodIncludes.js
+++ src/arrayMethodIncludes.js
@@ -5,7 +5,37 @@
  */
 function applyCustomIncludes() {
   [].__proto__.includes2 = function(valueToFind, fromIndex) {
-    // write code here
+    if (typeof valueToFind === 'undefined') {
+      return false;
+    }
+
+    let start;
+
+    if (fromIndex < 0) {
+      start = this.length - 1 + fromIndex;
+    } else {
+      start = fromIndex;
+    }
+
+    if (typeof fromIndex === 'undefined') {
+      start = 0;
+    }
+
+    if (fromIndex >= this.length) {
+      return false;
+    }
+
+    for (let i = start; i < this.length; i++) {
+      if (this[i] === valueToFind) {
+        return true;
+      }
+
+      if (isNaN(this[i]) && isNaN(valueToFind)) {
+        return true;
+      }
+    }
+
+    return false;
   };
 }
 
