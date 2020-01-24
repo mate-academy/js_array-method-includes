e3b5007463689208360e56133301ff32cefda7f2
Anastasiia Bardyuzha <stasya.meshcheryakova@gmail.com>
2020-01-24 21:05:51 +0200

    done

--- src/arrayMethodIncludes.js
+++ src/arrayMethodIncludes.js
@@ -5,7 +5,28 @@
  */
 function applyCustomIncludes() {
   [].__proto__.includes2 = function(valueToFind, fromIndex) {
-    // write code here
+    if (this.length === 0) {
+      return undefined;
+    }
+
+    const findValue = valueToFind || undefined;
+    let indexFrom = fromIndex || 0;
+
+    if (Math.sign(indexFrom) < 0) {
+      indexFrom = this.length - Math.abs(indexFrom);
+
+      if (Math.sign(indexFrom) < 0) {
+        indexFrom = 0;
+      }
+    }
+
+    for (let i = indexFrom; i < this.length; i++) {
+      if (this[i] === findValue) {
+        return true;
+      }
+    }
+
+    return false;
   };
 }
 
