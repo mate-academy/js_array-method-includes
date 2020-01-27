fa03d083833c0f3b4979f1bee5ea4bf45fff618c
Senya Volkov <formobileprima@gmail.com>
2020-01-27 02:01:56 +0200

    solution

--- src/arrayMethodIncludes.js
+++ src/arrayMethodIncludes.js
@@ -5,7 +5,25 @@
  */
 function applyCustomIncludes() {
   [].__proto__.includes2 = function(valueToFind, fromIndex) {
-    // write code here
+    let index;
+
+    if (Math.abs(fromIndex) !== fromIndex) {
+      index = (this.length + fromIndex) || 0;
+    } else {
+      index = fromIndex || 0;
+    }
+
+    if (index <= -1) {
+      index = 0;
+    }
+
+    for (let i = index; i < this.length; i++) {
+      if (Object.is(this[i], valueToFind)) {
+        return true;
+      }
+    }
+
+    return false;
   };
 }
 
