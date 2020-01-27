5c71c8158e95b8ae3407d83e27a684ffc9010729
Stanislav Mochyr <delenjer@gmail.com>
2020-01-27 16:13:11 +0200

    js_array-method-includes

--- src/arrayMethodIncludes.js
+++ src/arrayMethodIncludes.js
@@ -5,7 +5,47 @@
  */
 function applyCustomIncludes() {
   [].__proto__.includes2 = function(valueToFind, fromIndex) {
-    // write code here
+    if (fromIndex >= this.length) {
+      return false;
+    }
+
+    if (valueToFind === undefined) {
+      return false;
+    }
+
+    if (isNaN(valueToFind)) {
+      for (let i = 0; i < this.length; i++) {
+        if (isNaN(this[i])) {
+          return true;
+        }
+      }
+    }
+
+    if (fromIndex === undefined) {
+      for (let i = 0; i < this.length; i++) {
+        if (this[i] === valueToFind) {
+          return true;
+        }
+      }
+    }
+
+    if (fromIndex > 0) {
+      for (let i = fromIndex; i < this.length; i++) {
+        if (this[i] === valueToFind) {
+          return true;
+        }
+      }
+    }
+
+    if (fromIndex < 0) {
+      for (let i = this.length + fromIndex; i < this.length; i++) {
+        if (this[i] === valueToFind) {
+          return true;
+        }
+      }
+    }
+
+    return false;
   };
 }
 
