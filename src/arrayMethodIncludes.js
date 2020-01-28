94d09e2214dd4ad889a3203c6cf903bea72dc0b0
Valeriy-Sorochynskyi <haribol162@gmail.com>
2020-01-28 14:43:32 +0200

    includes2

--- src/arrayMethodIncludes.js
+++ src/arrayMethodIncludes.js
@@ -4,8 +4,31 @@
  * Implement method includes
  */
 function applyCustomIncludes() {
-  [].__proto__.includes2 = function(valueToFind, fromIndex) {
-    // write code here
+  [].__proto__.includes2 = function(valueToFind, fromIndex = 0) {
+    if (fromIndex > this.length) {
+      return false;
+    }
+
+    let searchFrom = fromIndex;
+
+    if (searchFrom < 0) {
+      searchFrom = this.length + fromIndex;
+    }
+
+    if (searchFrom < 0) {
+      searchFrom = 0;
+    }
+
+    for (let index = searchFrom; index < this.length; index++) {
+      if (
+        valueToFind === this[index]
+        || (Number.isNaN(valueToFind) && Number.isNaN(this[index]))
+      ) {
+        return true;
+      }
+    }
+
+    return false;
   };
 }
 
