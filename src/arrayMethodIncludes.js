59996a1414ac31e817598b0881cc39410c525c8a
Al145 <47367383+Al145@users.noreply.github.com>
2020-01-28 23:52:53 +0200

    solut

--- src/arrayMethodIncludes.js
+++ src/arrayMethodIncludes.js
@@ -4,8 +4,27 @@
  * Implement method includes
  */
 function applyCustomIncludes() {
-  [].__proto__.includes2 = function(valueToFind, fromIndex) {
+  [].__proto__.includes2 = function(valueToFind, fromIndex = 0) {
     // write code here
+    let include;
+
+    if (fromIndex < 0 && (this.length + fromIndex) < 0) {
+      include = 0;
+    } else if (fromIndex < 0) {
+      include = this.length + fromIndex;
+    } else {
+      include = fromIndex;
+    }
+
+    for (let i = include; i < this.length; i++) {
+      if ((this[i] === valueToFind)
+      || (Number.isNaN(valueToFind)
+      && Number.isNaN(this[i]))) {
+        return true;
+      }
+    }
+
+    return false;
   };
 }
 
