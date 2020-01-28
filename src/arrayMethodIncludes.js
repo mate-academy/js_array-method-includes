4fca4154960d52cd49d46876ccc74c4b53fe2590
Natalia Lypovyk <cardinnata@gmail.com>
2020-01-28 10:29:32 +0200

    default fromIndex added

--- src/arrayMethodIncludes.js
+++ src/arrayMethodIncludes.js
@@ -4,12 +4,18 @@
  * Implement method includes
  */
 function applyCustomIncludes() {
-  [].__proto__.includes2 = function(valueToFind, fromIndex) {
-    let index = fromIndex || 0;
+  [].__proto__.includes2 = function(valueToFind, fromIndex = 0) {
+    let index;
     const value = valueToFind || undefined;
 
     if (-fromIndex > this.length) {
       return false;
+    }
+
+    if (fromIndex < 0) {
+      index = this.length + fromIndex;
+    } else {
+      index = fromIndex;
     }
 
     if (fromIndex < 0) {
