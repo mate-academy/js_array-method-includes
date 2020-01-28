e42f9d809ac758e4ff001216786c800e49d5f0a6
Olexander <kravchenko.sania@gmail.com>
2020-01-28 23:44:17 +0200

    attempt 4

--- src/arrayMethodIncludes.js
+++ src/arrayMethodIncludes.js
@@ -5,12 +5,10 @@
  */
 function applyCustomIncludes() {
   [].__proto__.includes2 = function(valueToFind, fromIndex = 0) {
-    let beginingIndex;
+    let beginingIndex = fromIndex;
 
     if (fromIndex < 0) {
       beginingIndex = this.length + fromIndex < 0 ? 0 : this.length + fromIndex;
-    } else if (fromIndex >= 0) {
-      beginingIndex = fromIndex;
     }
 
     while (beginingIndex < this.length) {
