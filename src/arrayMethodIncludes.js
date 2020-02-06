e25f471391507a41e291e0322cfef956dbe5db45
ronyshchenko <roma.onyshchenko@gmail.com>
2020-02-06 16:50:47 +0200

    6-2-20-four

--- src/arrayMethodIncludes.js
+++ src/arrayMethodIncludes.js
@@ -4,8 +4,41 @@
  * Implement method includes
  */
 function applyCustomIncludes() {
-  [].__proto__.includes2 = function(valueToFind, fromIndex) {
-    // write code here
+  [].__proto__.includes2 = function(valueToFind, from = 0) {
+    let fromIndex = from;
+    let isIncludes = false;
+
+    if (fromIndex >= this.length) {
+      return false;
+    }
+
+    if (fromIndex <= (-1) * this.length) {
+      fromIndex = 0;
+    }
+
+    if (fromIndex >= 0) {
+      for (let i = fromIndex; i < this.length; i++) {
+        if (isNaN(this[i]) && isNaN(valueToFind)) {
+          return true;
+        } else if (this[i] === valueToFind) {
+          isIncludes = true;
+        }
+      }
+    }
+
+    if (fromIndex < 0) {
+      fromIndex = this.length + fromIndex;
+
+      for (let i = fromIndex; i < this.length; i++) {
+        if (isNaN(this[i]) && isNaN(valueToFind)) {
+          return true;
+        } else if (this[i] === valueToFind) {
+          isIncludes = true;
+        }
+      }
+    }
+
+    return isIncludes;
   };
 }
 
