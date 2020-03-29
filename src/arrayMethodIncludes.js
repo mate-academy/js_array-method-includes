b85ffb616f4fb836bf7d3decdd3000f422eae9af
crash2bash <sachagurov@gmail.com>
2020-03-29 12:57:20 +0300

    solution

--- src/arrayMethodIncludes.js
+++ src/arrayMethodIncludes.js
@@ -5,7 +5,34 @@
  */
 function applyCustomIncludes() {
   [].__proto__.includes2 = function(valueToFind, fromIndex) {
-    // write code here
+    let result;
+    let index = fromIndex;
+
+    if (index < 0) {
+      index += this.length;
+
+      if (index < -this.length) {
+        index = 0;
+      }
+    }
+
+    if (index !== undefined) {
+      return this[index] === valueToFind;
+    }
+
+    if (Number.isNaN(valueToFind) === Number.isNaN(NaN)) {
+      return true;
+    }
+
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] === valueToFind) {
+        return true;
+      } else {
+        result = false;
+      }
+    }
+
+    return result;
   };
 }
 
