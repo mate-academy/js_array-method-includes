74363ea899f519f141ffaad5bc975b313cc2c422
Andrey Faychak <faychak333@gmail.com>
2020-04-01 21:35:33 +0300

    'solution'

--- src/arrayMethodIncludes.js
+++ src/arrayMethodIncludes.js
@@ -6,6 +6,26 @@
 function applyCustomIncludes() {
   [].__proto__.includes2 = function(valueToFind, fromIndex) {
     // write code here
+    let withIndex = fromIndex;
+
+    if (fromIndex < -1 || fromIndex === undefined) {
+      withIndex = 0;
+    } else if (fromIndex >= this.length || fromIndex === -1) {
+      return false;
+    }
+
+    for (let i = withIndex; i < this.length; i++) {
+      if (typeof this[i] === 'number'
+        && isNaN(this[i])
+        && typeof valueToFind === 'number'
+        && isNaN(valueToFind)) {
+        return true;
+      } else if (this[i] === valueToFind) {
+        return true;
+      }
+    }
+
+    return false;
   };
 }
 
