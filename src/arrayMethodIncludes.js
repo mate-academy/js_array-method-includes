fcf78895458a2e6bce96f9436522d97b62c964aa
WincherZ <wincher2016@gmail.com>
2020-04-02 13:31:09 +0300

    some changes

--- src/arrayMethodIncludes.js
+++ src/arrayMethodIncludes.js
@@ -4,8 +4,25 @@
  * Implement method includes
  */
 function applyCustomIncludes() {
-  [].__proto__.includes2 = function(valueToFind, fromIndex) {
-    // write code here
+  [].__proto__.includes2 = function(valueToFind, fromIndex = 0) {
+    const arr = this;
+    let newIndex = fromIndex;
+
+    if (newIndex < 0) {
+      newIndex = newIndex + arr.length;
+    }
+
+    if (newIndex < 0) {
+      newIndex = 0;
+    }
+
+    for (let i = newIndex; i < arr.length; i++) {
+      if (Object.is(arr[i], valueToFind)) {
+        return true;
+      }
+    }
+
+    return false;
   };
 }
 
