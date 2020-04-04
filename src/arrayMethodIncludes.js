d937b0f1da5d2f131bd12de7ae0cd559dcb0d8e5
olenazubashenko <olenazubashenko@gmail.com>
2020-04-04 20:43:05 +0300

    task

--- src/arrayMethodIncludes.js
+++ src/arrayMethodIncludes.js
@@ -6,6 +6,19 @@
 function applyCustomIncludes() {
   [].__proto__.includes2 = function(valueToFind, fromIndex) {
     // write code here
+    let index = fromIndex;
+
+    if (index < 0) {
+      index = (this.length - 1) + index;
+    }
+
+    for (let i = 0; i < this.length; i++) {
+      if ((this[i] === valueToFind || Number.isNaN(this[i])) && i > index) {
+        return true;
+      }
+    }
+
+    return false;
   };
 }
 
