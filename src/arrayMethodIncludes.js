3d9ab3cb93c722c9edd3419cdc6e786428370b0a
Vitaliy <vitaliyanohin@gmail.com>
2020-04-17 12:23:27 +0300

    los

--- src/arrayMethodIncludes.js
+++ src/arrayMethodIncludes.js
@@ -5,7 +5,31 @@
  */
 function applyCustomIncludes() {
   [].__proto__.includes2 = function(valueToFind, fromIndex) {
-    // write code here
+    let index = fromIndex;
+
+    if (fromIndex === null | fromIndex === undefined) {
+      index = 0;
+    }
+
+    if (this.length + fromIndex < 0) {
+      index = 0;
+    } else if (fromIndex < 0) {
+      index = this.length + fromIndex;
+    }
+
+    for (index; index < this.length; index++) {
+      if (this[index] !== undefined
+        & isNaN(this[index])
+        & isNaN(valueToFind)) {
+        return true;
+      }
+
+      if (this[index] === valueToFind) {
+        return true;
+      }
+    }
+
+    return false;
   };
 }
 
