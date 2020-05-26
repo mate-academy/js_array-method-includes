a4e6713119ac6e02ecfb06f59787afc7907f7770
solaryasha <solaryasha@gmail.com>
2020-05-26 22:35:14 +0300

    Ugly version of my solution

--- src/arrayMethodIncludes.js
+++ src/arrayMethodIncludes.js
@@ -4,8 +4,30 @@
  * Implement method includes
  */
 function applyCustomIncludes() {
-  [].__proto__.includes2 = function(valueToFind, fromIndex) {
-    // write code here
+  [].__proto__.includes2 = function(valueToFind, fromIndex = 0) {
+    let newFromIndex = 0;
+
+    // how to handle negative correct index number
+    if (fromIndex < 0) {
+      if (this.length + fromIndex >= 0) {
+        newFromIndex += this.length;
+      } else {
+        return false;
+      }
+    }
+    // check for big negative fromIndex
+
+    for (let i = newFromIndex; i < this.length; i++) {
+      if (this[i] === valueToFind) {
+        return true;
+      } else if (isNaN(valueToFind)) {
+        if (isNaN(this[i])) {
+          return true;
+        }
+      }
+    }
+
+    return false;
   };
 }
 
