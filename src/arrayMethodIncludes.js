363887fe32ee7255a88621c7690bb59f68b006e0
solaryasha <solaryasha@gmail.com>
2020-05-26 23:25:19 +0300

    Fix variable names

--- src/arrayMethodIncludes.js
+++ src/arrayMethodIncludes.js
@@ -5,14 +5,15 @@
  */
 function applyCustomIncludes() {
   [].__proto__.includes2 = function(valueToFind, fromIndex = 0) {
-    const newFromIndex = fromIndex >= 0 ? fromIndex : fromIndex + this.length;
+    const changedFromIndex = fromIndex >= 0 ? fromIndex
+      : fromIndex + this.length;
 
-    if (this.length + newFromIndex < 0) {
+    if (this.length + changedFromIndex < 0) {
       return false;
     }
     // check for big negative fromIndex
 
-    for (let i = newFromIndex; i < this.length; i++) {
+    for (let i = changedFromIndex; i < this.length; i++) {
       if (Object.is(this[i], valueToFind)) {
         return true;
       }
