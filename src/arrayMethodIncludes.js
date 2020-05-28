6423508230d7057526669002e41ca7c83126aa36
Glib Rulev <velzepooz@gmail.com>
2020-05-28 17:05:52 +0300

    Solution

--- src/arrayMethodIncludes.js
+++ src/arrayMethodIncludes.js
@@ -4,8 +4,28 @@
  * Implement method includes
  */
 function applyCustomIncludes() {
-  [].__proto__.includes2 = function(valueToFind, fromIndex) {
-    // write code here
+  [].__proto__.includes2 = function(valueToFind, fromIndex = 0) {
+    let startIndex = fromIndex;
+
+    if (startIndex < 0) {
+      startIndex += this.length;
+
+      if (startIndex < 0) {
+        startIndex = 0;
+      }
+    }
+
+    for (let i = startIndex; i < this.length; i++) {
+      if (Number.isNaN(valueToFind) && Number.isNaN(this[i])) {
+        return true;
+      }
+
+      if (this[i] === valueToFind) {
+        return true;
+      }
+    }
+
+    return false;
   };
 }
 
