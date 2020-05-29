0fbc44c88faea21a8f34afbe706c5126710d840c
Yaroslav Andriyenko <andriyenko.yaroslav@gmail.com>
2020-05-29 17:20:32 +0300

    Solution

--- src/arrayMethodIncludes.js
+++ src/arrayMethodIncludes.js
@@ -4,8 +4,35 @@
  * Implement method includes
  */
 function applyCustomIncludes() {
-  [].__proto__.includes2 = function(valueToFind, fromIndex) {
-    // write code here
+  [].__proto__.includes2 = function(valueToFind, fromIndex = 0) {
+    if (this.length === 0 || fromIndex > this.length - 1) {
+      return false;
+    }
+
+    let searchedItem = valueToFind;
+
+    if (!searchedItem) {
+      searchedItem = undefined;
+    }
+
+    let i = 0;
+
+    if (fromIndex < 0 && (this.length + fromIndex) > 0) {
+      i = this.length + fromIndex;
+    } else if ((this.length + fromIndex) < 0) {
+      i = 0;
+    } else {
+      i = fromIndex;
+    }
+
+    for (i; i < this.length; i++) {
+      if (Object.is(this[i], searchedItem)
+        || (this[i] === null && searchedItem === null)) {
+        return true;
+      } else if (i === this.length - 1) {
+        return false;
+      }
+    }
   };
 }
 
