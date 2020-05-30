1b3a901d08c191973cb38e97511cae946a810690
DTR <t.terry.rice@gmail.com>
2020-05-30 17:24:19 +0300

    final 1

--- src/arrayMethodIncludes.js
+++ src/arrayMethodIncludes.js
@@ -1,11 +1,22 @@
 'use strict';
-
+/* eslint-disable */
 /**
  * Implement method includes
  */
+
 function applyCustomIncludes() {
   [].__proto__.includes2 = function(valueToFind, fromIndex) {
-    // write code here
+    if (fromIndex >= this.length) {return false};
+if (fromIndex < 0) {fromIndex = this.length + fromIndex};
+if (fromIndex < 0) {fromIndex = 0};
+
+for (let i = fromIndex || 0; i < this.length; i++) {
+  if(Object.is(valueToFind, this[i])) {
+    return true
+  };
+}
+
+return false;
   };
 }
 
