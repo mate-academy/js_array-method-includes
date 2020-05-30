b50c51d1925abba88cedc592f3e4c01c65ffae2e
tyooma <flying.alien.godzilla@gmail.com>
2020-05-30 21:13:36 +0300

    DIY Includes Method

--- src/arrayMethodIncludes.js
+++ src/arrayMethodIncludes.js
@@ -4,9 +4,24 @@
  * Implement method includes
  */
 function applyCustomIncludes() {
-  [].__proto__.includes2 = function(valueToFind, fromIndex) {
-    // write code here
+  [].__proto__.includes2 = function(valueToFind, fromIndex = 0) {
+    if (fromIndex < -this.length) {
+      // eslint-disable-next-line no-param-reassign
+      fromIndex = 0;
+    }
+
+    if (fromIndex < 0) {
+      // eslint-disable-next-line no-param-reassign
+      fromIndex = this.length + fromIndex;
+    }
+
+    for (let i = fromIndex; i < this.length; i++) {
+      if (Object.is(valueToFind, this[i])) {
+        return true;
+      }
+    }
+
+    return false;
   };
 }
-
 module.exports = applyCustomIncludes;
