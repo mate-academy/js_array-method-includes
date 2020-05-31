73148f06cada8c5e791e2829b4740cc36b6dd759
YanG-6668 <goch.yannn@gmail.com>
2020-05-31 13:18:40 +0300

    includes

--- src/arrayMethodIncludes.js
+++ src/arrayMethodIncludes.js
@@ -4,8 +4,29 @@
  * Implement method includes
  */
 function applyCustomIncludes() {
-  [].__proto__.includes2 = function(valueToFind, fromIndex) {
-    // write code here
+  [].__proto__.includes2 = function(valueToFind, fromIndex = 0) {
+    if (this.length === 0) {
+      return undefined;
+    }
+
+    let newIndex = fromIndex;
+
+    if (newIndex < 0) {
+      newIndex += this.length;
+
+      if (newIndex < 0) {
+        newIndex = 0;
+      }
+    }
+
+    for (let i = newIndex; i < this.length; i++) {
+      if (this[i] === valueToFind
+        || (Number.isNaN(valueToFind) && Number.isNaN(this[i]))) {
+        return true;
+      }
+    }
+
+    return false;
   };
 }
 
